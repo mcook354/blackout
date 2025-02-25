cbbce359d77e8aa32c091afa0ab8958e099716f7
Matt Cook <mattcook@Matts-Laptop.local>
2025-02-25 14:52:21 +0000

    Fix: CORS issue allowing Vercel frontend

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ app = FastAPI()
 # ✅ Enable CORS for React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://blackout-topaz.vercel.app/"],
+    allow_origins=["https://blackout-topaz.vercel.app/",
+                   "http://localhost:3000"
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

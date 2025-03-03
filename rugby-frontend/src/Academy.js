df2f5ee7060e433750edd675c463f8a289f1484a
Matt Cook <mattcook@Matts-Laptop.local>
2025-03-03 14:58:17 +0000

    Fix: Added basic Academy.js file to resolve module error

---
+++ rugby-frontend/src/Academy.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Academy = () => {
+  return (
+    <div>
+      <h2>Academy</h2>
+      <p>This section will display projected academy player skill levels.</p>
+    </div>
+  );
+};
+
+export default Academy;
